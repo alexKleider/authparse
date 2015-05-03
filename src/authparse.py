a52e3ab7b9443ce6b80987ee008bb42015b1bc51
Alex Kleider <alex@kleider.ca>
2015-05-03 09:55:27 -0700

    Get some functioning tests.
    Tests passing.  Want this commit before beginning to subdivide the
    Ip_Info class.

--- src/authparse.py
+++ src/authparse.py
@@ -1,4 +1,5 @@
 #!../venv/bin/python3
+#   print("Running Python3 script: 'authparse.py'.......")
 # -*- coding: utf-8 -*-
 # vim: set file encoding=utf-8 :
 #
@@ -20,73 +21,324 @@
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #   Look for file named COPYING.
 """
-Begin a TDD version of logparse limited only to auth.log
+A TDD version of logparse limited only to auth.log,
+'authparse.py' is typically used to process /var/log/auth.log files
+and retrieve IP addresses which can then be considered for filtering
+using iptables.
+When used without command line options: IP addresses are collected
+from STDIN and reported to STDOUT.  All IP addresses are collected and
+reported.  Additional information gleaned from data in the input
+auth.log file(s) can also be reported.
+
+usage:
+  logparser3.py --help
+  logparser3.py --version
+  logparser3.py  [-qkfd]
+                [-r | -rr ]
+                [--white <wfile>...]
+                [--black <bfile>...]
+                [--input <ifile>...]
+                [--output <ofile>]
+
+Options:
+  -h --help  Print the __doc__ string.
+  --version  Print the version number.
+  -r --report  How much to report about the IP addresses.
+          0 - Addresses alone.
+          1 - Addresses and number of times each one appeared.
+          2 - Addresses, number of appearances, type of appearances,
+              and additional information if available.
+  -d --demographics  Include location/origin of IP if possible.
+  -q --quiet  Supress reporting of success list, file access errors,
+              or files devoid of IPs.
+  -k --known  Report any known ('white' or 'black') IPs
+                that have been removed from output.
+  -w --white=<wfile>  Specify 0 or more files containing white listed IPs.
+  -b --black=<bfile>  Specify 0 or more files containing black listed IPs.
+  -i --input=<ifile>  Specify 0 or more input files.  If none are
+                      provided, stdin is used.
+                      These are typically auth.log files but don't have to be.
+                      If a specified file is a directory, all files
+                      with names ending in the suffix '.log' 
+                      beneath that directory are considered as though
+                      separately specified.
+  -o --output=<ofile>  Specify output file, otherwise std out is used.
+  -f --frequency   Sort output by frequency of appearance of IPs
+                   (Default is by IP.)
+
+Any known IPs can be provided in files specified as containing either
+'--black' or '--white' listed IPs.  These are also read and any IP
+addresses found will NOT be included in the output.  (See the
+-k/--known option which causes this to be reported.) Typically this
+would be useful if you have a 'white' list of known IPs you would
+definitely NOT want to block and/or if you had a 'black' list of already
+blocked IPs which you'd have no need to block again.
+Keep in mind that it should not be possible to have a black listed IP
+address appear in log files if it has in fact been blocked.
+
+If any provided file(s) don't exist or don't contain any IP's, this
+will be reported unless the -q/--quiet option is selected.
+
+If this scrolls too much, try piping to pager:
+./logparser3.py -h | pager
 """
-#
-# hash-bang line
-# encoding cookie
-# licence
-# doc string explaining what the module does
-# from __future__ imports
 # import standard library modules
-# import custom modules
+import os
+import re
+import shlex
 from pprint import pprint
+# import custom modules
 from docopt import docopt
 from ipwhois import IPWhois
 # metadata such as version number
+VERSION = "v0.0.0"
 # other constants
+# To retrieve (ipv4) IP addresses from a line:
+_IP_EXP = \
+r"""
+\b
+\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}
+"""
+_findall_ips = re.compile(_IP_EXP, re.VERBOSE).findall
 # global variables
-# custom exception types
+SPoL = dict(
+    invalid_user = dict(
+        re =  r"""Invalid user (?P<user>\S+) from """,
+        header_text =  "'auth.log' reporting 'invalid user's:",
+        keys =  ["user"]
+        ),
+    no_id = dict(
+        re =  (r"""Did not receive identification string from \S+"""),
+        header_text =  "'auth.log' reporting 'no id's:",
+        keys =   []
+        ),
+    break_in = dict(
+        re =  r"POSSIBLE BREAK-IN ATTEMPT!",
+        header_text =  (
+            "'auth.log' reporting 'POSSIBLE BREAK-IN ATTEMPT!'s:"),
+        keys =   []
+        ),
+    pub_key = dict(
+        re =  r""" Accepted publickey for (?P<user>\S+)""",
+        header_text =  "'auth.log' reporting ''s:",
+        keys =   ["user"]
+        ),
+    closed = dict(
+        re =  r""" Connection closed by \S+""",
+        header_text =  "'auth.log' reporting 'closed's:",
+        keys =   []
+        ),
+    disconnect = dict(
+        re =  (
+                r""" Received disconnect from """),
+        header_text =  (
+                "'auth.log' reporting 'Received disconnect from's:"),
+        keys =   []
+        ),
+    listening = dict(
+        re =  r""" Server listening on (?P<listener>.+)""",
+        header_text =  "'auth.log' reporting 'Server listening on's:",
+        keys =   ["listener"]
+        ),
+    not_allowed = dict(
+        re = r""" User (?P<user>\S+) from (?:\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) not allowed because none """,
+        header_text = "'auth.log' reporting ''s:",
+        keys = ["user"]
+        ),
+    unrecognized = dict(  # Besure this key is alphabetically last!
+        re = ".*",
+        header_text = "Unrecognized line.",
+        keys = []
+        ),
+    )
+
+
+_line_types = sorted(SPoL)
+# invalid_user, no_id, break_in, pub_key,
+# closed, disconnect, listening, not_allowed
+# unrecognized
+# noip
+
+for key in _line_types:
+    SPoL[key]['search4'] = re.compile(SPoL[key]['re'])
+
+# custom exception types  (I have none)
 # private functions and classes
 # public functions and classes
-def get_ip_info(ip_addr):
+
+class FileNameCollector(object):
+    def __init__(self, restrict2logs=False):
+        self.file_names = []
+        self.restrict2logs = restrict2logs
+    def add2list_of_file_names(self, f_or_dir_name):
+        f_or_dir_name = os.path.abspath(
+                            os.path.expanduser(
+                                f_or_dir_name))
+        if os.path.isfile(f_or_dir_name):
+            self.file_names.append(f_or_dir_name)
+        elif os.path.isdir(f_or_dir_name):
+            for path, dirs, files in os.walk(f_or_dir_name):
+                if self.restrict2logs:
+                    restricted_files = select_logs(files)
+                else:
+                    restricted_files = files
+                for f in restricted_files:
+                    self.file_names.append(os.path.join(path, f))
+        else:
+            print(
+            "Non-existing file name passed to FileNameCollector method.")
+
+class Ip_Info(object):
+    """An instance of this type is used to collect information about
+    each IP address that appears in the log (/var/log/authlog) files.
+    It is a dict keyed by the IP address.
+    Each value is also a dict keyed by the contents of _line_types
+    derived from SPoL in which it
+    appeared. Note there is also an "unrecognized" line type, the default
+    if none of the others match.
+    The corresponding value for each of these keys is a list, one for
+    each time that line type was encountered.  The list contents are in
+    turn a list of the values, often empty.  Summing the number of
+    lists for each line type provides a count for each IP address.
+    {ipaddr: 
+        {line_type:
+            { # dict, often empty, currently never > 1 key.
+            key(so far only "user" or "listener"):
+                    [list of names or other info bits],
+            }
+        }
+    }
+    """
+    def __init__(self):
+        data = {}
+    def add_entry(self, ip, info):
+        """<ip> is an IP address- key in top level of <data>.
+        <info> is the tuple returned by <get_log_info>:
+            [0]: line_type
+            [1]: group_dict: (often empty)
+                key (if any): ('user', 'listener', ...)
+                The value of each key is added to the list of values
+                for the corresponding key 
+        """
+        pass
+
+
+def get_args():
+    return docopt(__doc__, version=VERSION)
+
+def get_list_of_ips(line):
+    """Returns a list (possibly empty) of all ipv4 addresses found in
+    the line.
+    """
+    return _findall_ips(line)
+
+
+def get_ip_info(ip_addr, NO_INFO = "NO_INFO"):
+    """First parameter must be an IP address (as a dotted quad.)
+    Returns a dictionary with the following keys:
+        ip, description, city, address, state, country.
+    An alternative default value can be set in case the response
+    has no entry for one of the fields expected.
+    """
     obj = IPWhois(ip_addr)
     all_ip_info = obj.lookup()
     nets = all_ip_info['nets'][0]
     return dict(
-            ip = all_ip_info['query'],
-            address = nets.setdefault('address', 'unavailable'),
-            city= nets.setdefault('city', 'unavailable'),
-            country = nets.setdefault('country', 'unavailable'),
-            description = nets.setdefault('description', 'unavailable'),
-            state = nets.setdefault('state', 'unavailable'),
+            ip = all_ip_info.setdefault('query', NO_INFO),
+            address = nets.setdefault('address', NO_INFO),
+            city= nets.setdefault('city', NO_INFO),
+            country = nets.setdefault('country', NO_INFO),
+            description = nets.setdefault('description', NO_INFO),
+            state = nets.setdefault('state', NO_INFO),
             )
 
+def get_log_info(line):
+    """<line> is assumed to be a log file line.
+    Returns a tuple: line_type, data_gleaned.  
+        line_type: one of the keys of SPoL.
+        group_dic: a dictionary, possibly empty.
+             Currently there is never >1 item in the dictionary.
+             Possible keys are 'user', 'listener', ..
+             and only one value each. NOTE: in the IP_Info class
+             instance, the corresponding value is a list of values
+             collected by this procedure.
+    !!!Returns None if line is not a recognized log entry.
+    """
+    for line_type in _line_types :  # Assume a line can only be of 1 type.
+        search_result = SPoL[line_type]['search4'].search(line)
+        if search_result:  # Used search so will get 0 or 1 match object.
+            group_dic = {}
+            info_provided = SPoL[line_type]['keys']
+            for item in info_provided:  # May well be none.
+                group_dic[item] = search_result.groups(item)
+            return (line_type, group_dic, )
+
+def select_logs(list_of_files):
+    return [f_name for f_name in list_of_files if 'log' in f_name]
+
+def get_list_of_file_names(list_of_sources):
+    """
+    The parameter is an iterable of file (regular or directory) names.
+    Returned is a list of the full path names of all files.
+    """
+    file_name_collector = FileNameCollector()
+    for f_or_dir in list_of_sources:
+        file_name_collector.add2list_of_file_names(f_or_dir)
+    return file_name_collector.file_names
+
+def get_ips(list_of_sources):
+    """
+    The parameter is an iterable of file (regular or directory) names.
+    Returned is the set of all IP addresses contained in the files,
+    both those listed and those within directories listed.
+    The parameter would typically be the list of white or black files.
+    """
+    ret = set()
+    f_names = get_list_of_file_names(list_of_sources)
+    for f_name in f_names:
+        for line in open(f_name, 'r'):
+            ips = _findall_ips(line)
+            if ips:
+                for ip in ips:
+                    ret.append(ip)
+    return ret
+
+def get_ips_with_info(list_of_sources):
+    """
+    Parameter is an iterable of file (regular or directory) names.
+    Returned is a dict keyed by the first (if any) IP address found
+    in any line contained in the files.  Each key's value is an 
+    instance of IpInfo.
+    The parameter is typically the list of log files to analyse.
+    """
+    ret = {}
+    f_names = get_list_of_file_names(list_of_sources)
+    for f_name in f_names:
+        for line in open(f_name, 'r'):
+            ips = _findall_ips(line)
+            if ips:
+                ip = ips[0]
+                data = get_log_info(line)
+                if data:
+                    line_type, group_dict = data
+                else:
+                    line_type = 'unrecognized'
+                    group_dict = None
+    pass
+
 # main function
+def main():
+    white_collector = FileNameCollector()
+    for fname in args["--white"]:
+        white_collector.add2list_of_file_names(fname)
+    black_collector = FileNameCollector()
+    for fname in args["--black"]:
+        black_collector.add2list_of_file_names(fname)
+    logs_collector = FileNameCollector(True)
+    for fname in args["--in"]:
+        logs_collector.add2list_of_file_names(fname)
+
 #
 if __name__ == '__main__':  # code block to run the application
     ip_info = get_ip_info('76.191.204.54')
     pprint(ip_info)
-    pass
-#   print("Running Python3 script: 'authparse.py'.......")
-#   print('"""', end=''),
-#   print(__doc__, end=''),
-#   print('"""')
-
-    typical_output = """
-{'asn': '7065',
- 'asn_cidr': '76.191.192.0/19',
- 'asn_country_code': 'US',
- 'asn_date': '2007-09-10',
- 'asn_registry': 'arin',
- 'nets': [{'abuse_emails': 'abuse@sonic.net',
-           'address': '2260 Apollo Way',
-           'cidr': '76.191.128.0/17',
-           'city': 'Santa Rosa',
-           'country': 'US',
-           'created': '2007-09-10T00:00:00',
-           'description': 'SONIC.NET, INC.',
-           'handle': 'NET-76-191-128-0-1',
-           'misc_emails': None,
-           'name': 'SONIC-BLK',
-           'postal_code': '95407',
-           'range': '76.191.128.0 - 76.191.255.255',
-           'state': 'CA',
-           'tech_emails': 'noc@sonic.net',
-           'updated': '2012-03-02T00:00:00'}],
- 'query': '76.191.204.54',
- 'raw': None,
- 'raw_referral': None,
- 'referral': None}
-"""
-
--- t1.py
+++ t1.py
@@ -23,29 +23,215 @@
 First test of ./src/authparse.py
 """
 # import standard library modules
-# import custom modules
-from src.authparse import get_ip_info
+import re
+import random
 import unittest
+import shlex
+from pprint import pprint
+# import custom modules
+from docopt import docopt
+import support
+from src import authparse
 # metadata such as version number
 VERSION = "v0.0.0"
 # other constants
 # global variables
+
+# invalid_user, no_id, break_in, pub_key, closed, disconnect, listening
+log_dict = dict(
+    invalid_user = (
+'Mar  9 08:12:51 localhost sshd[4522]: Invalid user postgres from 202.153.165.67'),
+    no_id = (
+'Mar  9 06:48:15 localhost sshd[4513]: Did not receive identification string from 1.93.29.129'),
+    break_in = (
+'Apr 27 00:43:04 localhost sshd[16849]: reverse mapping checking getaddrinfo for canariosdelmundo.com [87.106.173.118] failed - POSSIBLE BREAK-IN ATTEMPT!'),
+    pub_key = (
+'Apr 28 03:52:20 localhost sshd[21641]: Accepted publickey for alex from 76.191.204.54 port 61583 ssh2'),
+    closed = (
+'Mar  9 09:47:15 localhost sshd[4561]: Connection closed by 61.174.51.208 [preauth]'),
+    disconnect = (
+'Mar  9 08:12:56 localhost sshd[4526]: Received disconnect from 202.153.165.67: 11: Bye Bye [preauth]'),
+    listening = (
+''),
+    # Couldn't find a 'Server listening on' line. 
+    not_allowed = (
+"Apr 26 19:04:31 localhost sshd[16553]: User root from 222.186.134.98 not allowed because none of user's groups are listed in AllowGroups"),
+      )
+
 # custom exception types
 # private functions and classes
 # public functions and classes
-class IPInfoTest(unittest.TestCase):
+class InfoTest(unittest.TestCase):
+    """Tests VERSION,
+            _IP_EXP
+            get_ip_info
+    """
+    test_ips = ["76.191.204.54",   # by Sonic.net dhcp in Bolinas
+# Commented out to save time.
+#              "173.228.54.112",   # by Sonic.net dhcp in RWC
+#              "204.14.156.167",   # by WebPass in San Francisco
+#              "89.18.173.13",     # by pcExtreme in Amsterdam
+                        ]
+    def test_version(self):
+        self.assertEqual(VERSION, authparse.VERSION)
+        self.assertEqual(VERSION, support.VERSION)
     def test_get_ip_info(self):
-        ip = "76.191.204.54"
-        ip_info = get_ip_info(ip)
-        assert ip_info['ip'] == ip
+        ips = InfoTest.test_ips
+        for ip in ips:
+            ip_info = authparse.get_ip_info(ip)
+            self.assertEqual(ip_info['ip'], ip)
+            pprint(ip_info)
+    def test_IP_EXP(self):
+        line = (
+        'My IP in Bolinas is 76.191.204.54 provided by Sonic.net.')
+        regex = re.compile(authparse._IP_EXP, re.VERBOSE)
+        self.assertRegexpMatches(line, regex)
+
+class LineTest(unittest.TestCase):
+    """Tests get_list_of_ips, 
+            get_log_info
+    """
+    def test_get_list_of_ips(self):
+        for i in range(10):
+            for n_ips in range(4):
+                rl = support.get_random_line(40 + i)
+                ips = [support.get_rand_ip() for i in range(n_ips)]
+                test_line = support.insert(ips,
+                                rl,
+                                support.random_locations(n_ips, 40))
+                list_of_ips = authparse.get_list_of_ips(test_line)
+                self.assertEqual(len(list_of_ips), n_ips)
+    def test_get_log_info(self):
+        res = authparse.get_log_info(log_dict['invalid_user'])
+        self.assertEqual(res[0], 'invalid_user')
+        self.assertEqual(res[1]["user"][0], "postgres")
+        res = authparse.get_log_info(log_dict['no_id'])
+        self.assertEqual(res[0], 'no_id')
+        print(res[1])
+        self.assertEqual(res[1], {})
+        res = authparse.get_log_info(log_dict['break_in'])
+        self.assertEqual(res[0], 'break_in')
+        self.assertEqual(res[1], {})
+        res = authparse.get_log_info(log_dict['pub_key'])
+        self.assertEqual(res[0], 'pub_key')
+        self.assertEqual(res[1]["user"][0], "alex")
+        res = authparse.get_log_info(log_dict['closed'])
+        self.assertEqual(res[0], 'closed')
+        self.assertEqual(res[1], {})
+        res = authparse.get_log_info(log_dict['disconnect'])
+        self.assertEqual(res[0], 'disconnect')
+        self.assertEqual(res[1], {})
+    #   res = authparse.get_log_info(log_dict['listening'])
+    #   self.assertEqual(res[0], 'listening')
+    #   self.assertEqual(res[1], {'server'})
+        res = authparse.get_log_info(log_dict['not_allowed'])
+        self.assertEqual(res[0], 'not_allowed')
+        self.assertEqual(res[1]["user"][0], "root")
+
+class FilesTest(unittest.TestCase):
+    """Tests: select_logs
+    """
+    test_list_of_logfiles = ['a.log', 'auth.log', 'auth.log.1',
+                            'auth.log.2', 'auth.log.3',
+                            'auth.log.4', 'fail.log', 'beaware',]
+    def test_select_logs(self):
+        file_names = FilesTest.test_list_of_logfiles
+        files = authparse.select_logs(file_names)
+        non_log = ['beaware',]
+        qualified = (
+        [name for name in file_names if not name in non_log])
+        self.assertEqual(files, qualified)
+
+class ArgsTest(unittest.TestCase):
+    """Test the command line argument/docopt componenet:
+    def test_get_args0(self):  # template
+        cmd = "" 
+        c_l_args = shelx.split(cmd)
+        args = docopt(authparse.__doc__,
+                        argv=c_l_args[1:],
+                        help=True,
+                        version=VERSION,
+                        options_first=False)
+        assert args['--help'] == 
+        assert args['--version'] ==
+        assert args['--report'] == 
+        assert args['--demographics'] == 
+        assert args['--quiet'] == 
+        assert args['--known'] == 
+        assert args['--white'] == []
+        assert args['--black'] == []
+        assert args['--input'] == []
+        assert args['--output'] == 
+        assert args['--frequency'] == 
+    """
+    def test_get_args3(self):
+        cmd ="argparser.py -rdqki auth.log -o output.txt" 
+        c_l_args = shlex.split(cmd)
+        args = docopt(authparse.__doc__,
+                        argv=c_l_args[1:],
+                        help=True,
+                        version=VERSION,
+                        options_first=False)
+        self.assertEqual(args['--help'], False)
+        self.assertEqual(args['--version'], False)
+        self.assertEqual(args['--report'],  1)
+        self.assertEqual(args['--demographics'], True)
+        self.assertEqual(args['--quiet'], True)
+        self.assertEqual(args['--known'], True)
+        self.assertEqual(args['--white'], [])
+        self.assertEqual(args['--black'], [])
+        self.assertEqual(args['--input'], ['auth.log'])
+        self.assertEqual(args['--output'], 'output.txt')
+        self.assertEqual(args['--frequency'], False)
+
+class TestFileNameCollector(unittest.TestCase):
+    """Tests/exercises: FileNameCollector
+            it's add2list_of_file_names method,
+            and it's file_names attribute.
+    """
+    def test_dir_of_logs0(self):
+        cmd ="argparser.py -i '~/Py/Logparse/DD/Logs' -o output.txt" 
+        c_l_args = shlex.split(cmd)
+        args = docopt(authparse.__doc__,
+                        argv=c_l_args[1:],
+                        help=True,
+                        version=VERSION,
+                        options_first=False)
+        log_file_collector = authparse.FileNameCollector()
+        for dir_or_file in args["--input"]:
+            log_file_collector.add2list_of_file_names(dir_or_file)
+        self.assertEqual(sorted(log_file_collector.file_names), sorted([
+            "/home/alex/Py/Logparse/DD/Logs/a.log",
+            "/home/alex/Py/Logparse/DD/Logs/auth.log",
+            "/home/alex/Py/Logparse/DD/Logs/auth.log.1",
+            "/home/alex/Py/Logparse/DD/Logs/auth.log.2",
+            "/home/alex/Py/Logparse/DD/Logs/auth.log.3",
+            "/home/alex/Py/Logparse/DD/Logs/auth.log.4",
+            "/home/alex/Py/Logparse/DD/Logs/fail.log",
+            "/home/alex/Py/Logparse/DD/Logs/not_l_o_g_file",
+            "/home/alex/Py/Logparse/DD/Logs/SubLog/SubLogSubLog/subsub.log",
+            "/home/alex/Py/Logparse/DD/Logs/SubLog/SubLogSubLog/subsub.log.1",
+            "/home/alex/Py/Logparse/DD/Logs/SubLog/SubLogSubLog1/subsub1.log",
+            "/home/alex/Py/Logparse/DD/Logs/SubLog/SubLogSubLog1/subsub1.log.1",
+                ]))
+        log_file_collector = authparse.FileNameCollector(True)
+        log_file_collector.add2list_of_file_names(dir_or_file)
+        self.assertEqual(sorted(log_file_collector.file_names), sorted([
+            "/home/alex/Py/Logparse/DD/Logs/a.log",
+            "/home/alex/Py/Logparse/DD/Logs/auth.log",
+            "/home/alex/Py/Logparse/DD/Logs/auth.log.1",
+            "/home/alex/Py/Logparse/DD/Logs/auth.log.2",
+            "/home/alex/Py/Logparse/DD/Logs/auth.log.3",
+            "/home/alex/Py/Logparse/DD/Logs/auth.log.4",
+            "/home/alex/Py/Logparse/DD/Logs/fail.log",
+            "/home/alex/Py/Logparse/DD/Logs/SubLog/SubLogSubLog/subsub.log",
+            "/home/alex/Py/Logparse/DD/Logs/SubLog/SubLogSubLog/subsub.log.1",
+            "/home/alex/Py/Logparse/DD/Logs/SubLog/SubLogSubLog1/subsub1.log",
+            "/home/alex/Py/Logparse/DD/Logs/SubLog/SubLogSubLog1/subsub1.log.1",
+                ]))
 
 # main function
 #
 if __name__ == '__main__':  # code block to run the application
     pass
-#   print("Running Python3 script: 't1.py'.......")
-#   print('"""', end=''),
-#   print(__doc__, end=''),
-#   print('"""')
-
 
